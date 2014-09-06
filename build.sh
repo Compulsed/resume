5dcdb1bc9184e554ad0dbffa93b46a9ebaf6ad89
Dale Salter <djsalter93@hotmail.com>
2014-09-06 17:03:07 +1000

    ADDED: Cover letter

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 cd src;
-lualatex dalesalter;
-mv dalesalter.pdf ../;
+
+pdflatex --shell-escape $1;
+lualatex $2;
+
+mv $1.pdf ../;
+mv $2.pdf ../;
+
 cd ../;
+
+gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=$3.pdf -dBATCH $1.pdf $2.pdf;
