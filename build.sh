fe290bd713ac1c1bda511e31850c53016740b791
Dale Salter <djsalter93@hotmail.com>
2014-09-06 18:25:23 +1000

    UPADED: Change build script to correctly accept arguments

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+# Check if there are three arguments
+if [ $# -eq 3 ]; then
+    # Check if the input files actually exist 
+    if ! [[ -f "src/$1.tex" ]]; then 
+       echo "The input file $1.tex does not exist."
+       exit 1
+    fi
+
+    if ! [[ -f "src/$2.tex" ]]; then 
+       echo "The input file $2.tex does not exist." 
+       exit 1 
+    fi 
+else
+   echo "Usage: $0 [input tex] [intput tex] [output]"
+   exit 1
+fi
+  
+# Creates the merged PDF
 cd src;
 
 pdflatex --shell-escape $1;
